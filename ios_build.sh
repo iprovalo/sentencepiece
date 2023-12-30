63874c9f5631036b1f48ad4f88e065043568dac7
ivan provalov <iprovalo@yahoo.com>
2023-12-30 08:52:45 -0800

    Building for iOS
    Building for iOS

---
+++ ios_build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#./ios_build.sh build clean
+
+set -e
+TEAM_ID="Ivan Provalov"
+
+if [ "$1" == "help" ]; then
+  echo "Run bash ios_build.sh build clean"
+  echo "Run bash ios_build.sh version"
+#  echo "Go to Xcode Archive Organizer and upload!"
+  exit
+fi
+
+if [ "$1" == "build" ] || [ "$1" == "configure" ]; then
+echo "Running CMake configuration..."
+
+# clean up old builds
+if [ "$2" == "clean" ]; then rm -Rf build-ios; fi
+
+# generate new builds
+cmake -Bbuild-ios -GXcode -DCMAKE_SYSTEM_NAME=iOS \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
+    -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM="$TEAM_ID"
+fi
