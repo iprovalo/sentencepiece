e5580cc25810a8509dee5c9834526acb5c919959
ivan provalov <iprovalo@yahoo.com>
2023-12-30 09:29:01 -0800

    Instructions for iOS

--- ios_build.sh
+++ ios_build.sh
@@ -3,6 +3,17 @@
 set -e
 TEAM_ID="Ivan Provalov"
 
+# This will configure cmake build.  After this step, open the project in XCode and you may need to make some changes for the build time:
+#1. Build Settings -> Other Linker Flags - make sure has the file names as libsentencepiece.0.0.0 for @rpath
+#2. Check iphone and iPad under General on each specific target - this may not be required.
+#3. PROJECT LEVEL - Build Settings - Build Active Architecture Only - NO
+#4. Build both sentencepiece and sentencepiece_train separately
+#5. Open build-os folder containing dylib in Finder
+#6. Drag and drop the dylib in XCode (Do not copy) to you target app project
+#7. Make sure these are now linked to the Target in both Binary and Embed Libraries under Build Phases of the specific target
+#8. Make sure these are findable - Build Settings of the specific target - Header and Library Search Paths include recursive references to the header folder src and build-ios folder of the sentencepiece
+
+
 if [ "$1" == "help" ]; then
   echo "Run bash ios_build.sh build clean"
   exit
